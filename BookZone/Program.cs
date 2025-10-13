fe7ba1d540feaed7f030c1222124a9bb0d3dd0a4
fares2718 <faresobaid2715@gmail.com>
2025-10-13 15:54:12 +0300

    Add EF and DbContext

--- BookZone/Program.cs
+++ BookZone/Program.cs
@@ -1,6 +1,14 @@
+using BookZone.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var ConnectionString = builder.Configuration.GetConnectionString("DefualtConnection") 
+                                            ?? throw new InvalidOperationException("No connection was found");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(ConnectionString));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
