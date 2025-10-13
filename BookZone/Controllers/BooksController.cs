6c4e88c2cf95431293c356ace4fe3c6dd929134a
fares2718 <faresobaid2715@gmail.com>
2025-10-13 17:14:55 +0300

    Add Books Controller

---
+++ BookZone/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookZone.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Creat()
+        {
+            return View();
+        }
+    }
+}
