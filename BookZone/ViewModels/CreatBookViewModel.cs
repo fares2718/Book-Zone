c7b8e10fa8780aa8bcee29ba2fdf3776b5bd21d6
fares2718 <faresobaid2715@gmail.com>
2025-10-14 11:40:52 +0300

    Add the Add New Form View Model

---
+++ BookZone/ViewModels/CreatBookViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookZone.ViewModels
+{
+    public class CreatBookViewModel
+    {
+        [MaxLength(250)]
+        public string Name { get; set; } = string.Empty;
+        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
+        public IEnumerable<SelectListItem> Categories = Enumerable.Empty<SelectListItem>();
+        public List<int> SelectedLanguages { get; set; } = new List<int>();
+        public IEnumerable<SelectListItem> Languages = Enumerable.Empty<SelectListItem>();
+        [MaxLength(2500)]
+        public string Description { get; set; } = string.Empty;
+        public IFormFile Cover { get; set; } = default!;
+    }
+}
